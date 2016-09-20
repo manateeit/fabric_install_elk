f348c03ada83bc8ad365394ce6026c0e2b539cae
Chris McKenna <chris.mediathread@gmail.com>
2016-09-20 09:16:49 -0400

    more updates

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
 from fabric.api import *
 from fabric.contrib.files import exists
+import time
 
 
 @task
@@ -9,12 +10,17 @@ def installJava8():
     run("./installJava8.sh")
     run("java -version")
 
+@task
+def updateRepo():
+    # run("wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -")
+    # run('echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list')
+    # run('echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list')
+    run("echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash-2.2.x.list")
+    run("sudo apt-get update")
+
 
 @task
 def installElasticSearch():
-    run("wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -")
-    run('echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list')
-    run("sudo apt-get update")
     run("sudo apt-get -y install elasticsearch")
     put("./elasticsearch.yml","/etc/elasticsearch/elasticsearch.yml", use_sudo=True)
     restartElasticSearch()
@@ -22,18 +28,32 @@ def installElasticSearch():
 
 @task 
 def installKibana():
-    # run('echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list')
-    # run("sudo apt-get update")
-    # run("sudo apt-get -y install kibana")
+    run("sudo apt-get -y install kibana")
     put("./kibana.yml" , "/opt/kibana/config/kibana.yml", use_sudo=True)
     restartKibana()
     run("sudo update-rc.d kibana defaults 96 9")
+
+@task 
+def installNginx():
+    run("sudo apt-get -y install nginx apache2-utils")
+    sudo("sudo htpasswd -c /etc/nginx/htpasswd.users kibanaadmin")
+    put("nginxSitesDefault", "/etc/nginx/sites-available/default", use_sudo=True)
+    restartNginx()
+
+@task
+def installLogstash():
+    updateRepo()
+    run("sudo apt-get install logstash")
+    run("cd /etc/pki/tls; sudo openssl req -subj '/CN=elkpri.itmanaged.solutions/' -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt")
+    get("/etc/logstash/conf.d/02-beats-input.conf", ".", use_sudo=True)
+    
 
 
 
 @task
 def restartElasticSearch():
     stopElasticSearch()
+    time.sleep(5)
     startElasticSearch()
 
 @task 
@@ -47,7 +67,10 @@ def startElasticSearch():
 @task
 def restartKibana():
     stopKibana()
+    time.sleep(5)
     startKibana()
+    time.sleep(5)
+    statusKibana()
 
 @task 
 def stopKibana():
@@ -56,8 +79,25 @@ def stopKibana():
 @task 
 def startKibana():
       run("sudo service kibana start")
+
+@task 
+def statusKibana():
+      run("sudo service kibana status")
+
+@task
+def restartNginx():
+    stopNginx()
+    time.sleep(5)
+    startNginx()
   
 
+@task 
+def stopNginx():
+    run("sudo service nginx stop")
+    
+@task 
+def startNginx():
+      run("sudo service nginx start")
 
 
 
